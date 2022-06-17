6b46fb991eefe68f54460fb708415008b1fc36f7
Harsha Kumar <harzkr142@gmail.com>
2022-06-17 20:18:54 +0530

    Update essage structure and function

---
+++ src/services/message.service.js
@@ -0,0 +1,45 @@
+const httpStatus = require('http-status');
+const { getConversation } = require('../controllers/conversation.controller');
+const { Message } = require('../models');
+const ApiError = require('../utils/ApiError');
+
+/**
+ * Create a message
+ * @param {Object} messageBody
+ * @returns {Promise<Message>}
+ */
+const createMessage = async (messageBody) => {
+  if (await getConversation(messageBody.conversation)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Conversation not found');
+  }
+  return Message.create(messageBody);
+};
+
+/**
+ * Query for users
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const queryMessages = async (filter, options) => {
+  const messages = await Message.paginate(filter, options);
+  return messages;
+};
+
+/**
+ * Get user by id
+ * @param {ObjectId} id
+ * @returns {Promise<Message>}
+ */
+const getMessage = async (id) => {
+  return Message.findById(id);
+};
+
+module.exports = {
+  createMessage,
+  queryMessages,
+  getMessage,
+};
