1c6616eed562fbf4897511b7788dc4a81beaa4d0
Harsha Kumar <harzkr142@gmail.com>
2022-06-17 20:37:03 +0530

    conversation service update

---
+++ src/services/conversation.service.js
@@ -0,0 +1,77 @@
+const httpStatus = require('http-status');
+const { Conversation } = require('../models');
+const ApiError = require('../utils/ApiError');
+
+/**
+ * Create a user
+ * @param {Object} conversationBody
+ * @returns {Promise<Conversation>}
+ */
+const createConversation = async (conversationBody) => {
+  return Conversation.create(conversationBody);
+};
+
+/**
+ * Query for users
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const queryConversations = async (filter, options) => {
+  const conversations = await Conversations.paginate(filter, options);
+  return conversations;
+};
+
+/**
+ * Get user by id
+ * @param {ObjectId} id
+ * @returns {Promise<User>}
+ */
+const getConversation = async (id) => {
+  return Conversation.findById(id);
+};
+
+/**
+ * Update user by id
+ * @param {ObjectId} userId
+ * @param {Object} updateBody
+ * @returns {Promise<User>}
+ */
+const updateConv = async (userId, updateBody) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  if (updateBody.email && (await User.isEmailTaken(updateBody.email, userId))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  }
+  Object.assign(user, updateBody);
+  await user.save();
+  return user;
+};
+
+/**
+ * Delete user by id
+ * @param {ObjectId} userId
+ * @returns {Promise<User>}
+ */
+const deleteConversation = async (conversationId) => {
+  const conversation = await getConversation(conversationId);
+  if (!conversation) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Conversation not found');
+  }
+  await conversation.remove();
+  return conversation;
+};
+
+module.exports = {
+  createUser,
+  queryUsers,
+  getUserById,
+  getUserByEmail,
+  updateUserById,
+  deleteUserById,
+};
