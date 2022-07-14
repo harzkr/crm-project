27c6272ec2aa2049cef32131f4529b02f7a0cec8
Harsha Kumar <harzkr142@gmail.com>
2022-07-14 13:58:41 +0530

    Specify origins

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const composedLog = R.compose(console.log, separator);
 
 const io = new Server(serverInterface,{
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
   }
 });
 
